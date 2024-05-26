ffafd0fb6fb44ae1c40629381a98902f987fcfd0
Hilton W. Silva <me@hilton.ws>
2024-05-26 12:37:22 +0100

    Update

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -122,7 +122,7 @@ class AskIntentHandler(AbstractRequestHandler):
                 ],
                 model=model,
             )
-            speak_output = response.choices[0].message.strip()
+            speak_output = response.choices[0].message.content.strip()
 
         return (
             handler_input.response_builder
